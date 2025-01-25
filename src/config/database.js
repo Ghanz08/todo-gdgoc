8da99ea7ad6d85f753728bf7ae8da76026f41101
Ghanz08 <ghanizulhusnib@gmail.com>
2025-01-25 18:13:39 +0700

    test

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,8 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}/${process.env.MYSQLDATABASE}`;
+const pool = mysql.createPool(urlDB);
 
 // Convert pool to use promises
 const promisePool = pool.promise();
