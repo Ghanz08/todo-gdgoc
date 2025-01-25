54a60c04829b921184aeb6e2cc63e38bfce40fa7
Ghanz08 <ghanizulhusnib@gmail.com>
2025-01-25 18:30:24 +0700

    t

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,8 @@
 const mysql = require('mysql2');
-require('dotenv').config();
 
 const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}/${process.env.MYSQLDATABASE}`;
 const pool = mysql.createPool(urlDB);
 
-// Convert pool to use promises
 const promisePool = pool.promise();
 
 module.exports = promisePool;
